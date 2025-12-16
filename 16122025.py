fd45c524d051ddbf13c24c551d068d7e728e1627
Rajesh A <rajuraj0706@gmail.com>
2025-12-16 20:03:39 +0530

    used set instead of list in consonant count

--- 16122025.py
+++ 16122025.py
@@ -1,7 +1,8 @@
 def has_consonant_count(text, target):
     count = 0
+    vowels = {'a','e','i','o','u'}
     for char in text:
-        if char.isalpha() and  char.lower() not in ['a','e','i','o','u']:
+        if char.isalpha() and  char.lower() not in vowels:
             count += 1
     if count == target:
         return True
